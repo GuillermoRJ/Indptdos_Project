2d88eb646862666a50593e4f649d078f43172f8b
GuillermoRJ <g.rdz.jauregui06@gmail.com>
2026-07-09 23:32:19 -0600

    chance e2e #2

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -6,7 +6,7 @@ class TestEcommerceE2E:
 
   # --- ESCENARIOS BASE DE MEMO (Puntos 1, 2, 3) ---
 
-  def test_login_invalid_credentials2(self, driver):
+  def test_login_valid_credentials(self, driver):
     login_page = LoginPage(driver)
     login_page.load()
     login_page.login("no@test.com", "no")
@@ -20,23 +20,6 @@ class TestEcommerceE2E:
     error_msg = login_page.get_error_message()
     assert len(error_msg) > 0, "Debería mostrarse un mensaje de error."
 
-  def test_search_empty_string(self, driver):
-    home_page = HomePage(driver)
-    home_page.load()
-    home_page.search("")
-    count = home_page.get_product_count()
-    assert count > 0, "Deberían mostrarse productos por defecto al buscar vacío."
-
-  def test_search_nonexistent_product(self, driver):
-    home_page = HomePage(driver)
-    home_page.load()
-    home_page.search("ProductoFalsoQueNoExiste999")
-    msg = home_page.get_no_results_message()
-    assert "no" in msg.lower() or "encontrado" in msg.lower(), "Debería indicar que no hay resultados."
-
-  # --- TU APORTE - JORGE (Punto 4: Pruebas Data-Driven con @pytest.mark.parametrize) ---
-
-  # Escenario Parametrizado A: Validaciones de campos vacíos y errores de Login
   @pytest.mark.parametrize("username, password, expected_error", [
     ("", "admin123", "El usuario es requerido"),
     ("jorge@test.com", "", "La contraseña es requerida"),
