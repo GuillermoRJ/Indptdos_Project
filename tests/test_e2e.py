10bf212170a07d084461bc28cbba57baf25ad719
GuillermoRJ <g.rdz.jauregui06@gmail.com>
2026-07-09 23:37:12 -0600

    change e2e #3

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -25,22 +25,9 @@ class TestEcommerceE2E:
     ("jorge@test.com", "", "La contraseña es requerida"),
     ("usuario_invalido", "pass", "incorrectas")
   ])
-  def test_login_validation_data_driven(self, driver, username, password, expected_error):
+  def test_login_validation_data_driven(self, driver, username, password):
     login_page = LoginPage(driver)
     login_page.load()
     login_page.login(username, password)
     error_msg = login_page.get_error_message()
     assert len(error_msg) > 0, "Debería lanzar un mensaje de validación en el formulario."
-
-  # Escenario Parametrizado B: Búsqueda múltiple de productos existentes en el catálogo
-  @pytest.mark.parametrize("producto, cantidad_esperada", [
-    ("Small Aluminum Fish", 1),
-    ("Refined Granite Towels", 1),
-    ("Bespoke Cotton Table", 1)
-  ])
-  def test_search_existing_products_data_driven(self, driver, producto, cantidad_esperada):
-    home_page = HomePage(driver)
-    home_page.load()
-    home_page.search(producto)
-    count = home_page.get_product_count()
-    assert count >= cantidad_esperada, f"Fallo al encontrar el producto: {producto}"
