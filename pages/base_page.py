d42e3c12f5b8614ce67d9f97657a1bc288b2d8ce
GuillermoRJ <g.rdz.jauregui06@gmail.com>
2026-07-09 18:37:50 -0600

    Update: real data in e2e

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,19 +4,15 @@ from selenium.webdriver.support import expected_conditions as EC
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
-        self.wait = WebDriverWait(self.driver, 10) # Espera explícita máxima de 10 segundos
+        # Espera dinámica para elementos renderizados en Angular
+        self.wait = WebDriverWait(self.driver, 10)
 
-    def find(self, *locator):
+    def find_element(self, *locator):
         return self.wait.until(EC.presence_of_element_located(locator))
 
-    def click(self, *locator):
+    def find_elements(self, *locator):
+        return self.wait.until(EC.presence_of_all_elements_located(locator))
+
+    def click_element(self, *locator):
         element = self.wait.until(EC.element_to_be_clickable(locator))
         element.click()
-
-    def type_text(self, text, *locator):
-        element = self.find(*locator)
-        element.clear()
-        element.send_keys(text)
-        
-    def get_text(self, *locator):
-        return self.find(*locator).text
--- pages/login_page.py
+++ pages/login_page.py
@@ -2,21 +2,25 @@ from selenium.webdriver.common.by import By
 from .base_page import BasePage
 
 class LoginPage(BasePage):
-    URL = "http://localhost:4200/login" # Ajusta a la ruta real de tu frontend Angular
+    URL = "http://localhost:4200/login"
     
-    # Localizadores (Ajústalos a los selectores reales de tu app)
-    USERNAME_INPUT = (By.ID, "username")
-    PASSWORD_INPUT = (By.ID, "password")
-    LOGIN_BUTTON = (By.CSS_SELECTOR, "button[type='submit']")
-    ERROR_MESSAGE = (By.CSS_SELECTOR, ".error-msg")
+    # Selectores ajustados a los directivas de Angular
+    EMAIL_INPUT = (By.CSS_SELECTOR, "input[formControlName='email']")
+    PASSWORD_INPUT = (By.CSS_SELECTOR, "input[formControlName='password']")
+    SUBMIT_BUTTON = (By.CSS_SELECTOR, "button[type='submit']")
+    ERROR_MESSAGE = (By.CSS_SELECTOR, "app-error-message")
 
     def load(self):
         self.driver.get(self.URL)
 
-    def login(self, username, password):
-        self.type_text(username, *self.USERNAME_INPUT)
-        self.type_text(password, *self.PASSWORD_INPUT)
-        self.click(*self.LOGIN_BUTTON)
+    def login(self, email, password):
+        self.find_element(*self.EMAIL_INPUT).clear()
+        self.find_element(*self.EMAIL_INPUT).send_keys(email)
         
-    def get_error_message(self):
-        return self.get_text(*self.ERROR_MESSAGE)
+        self.find_element(*self.PASSWORD_INPUT).clear()
+        self.find_element(*self.PASSWORD_INPUT).send_keys(password)
+        
+        self.click_element(*self.SUBMIT_BUTTON)
+        
+    def get_error_text(self):
+        return self.find_element(*self.ERROR_MESSAGE).text
